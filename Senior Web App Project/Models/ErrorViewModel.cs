287904328744d61baaa11471d76817fc3e153aeb
JayT <jayt7631@gmail.com>
2019-09-22 14:21:56 -0400

    Add project files.

---
+++ Senior Web App Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Senior_Web_App_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
